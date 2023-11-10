b5d44daa6e24d665829c737862b89521d9f3d6df
PTBYSR <paul.emechebe@gmail.com>
2023-11-10 14:35:35 +0100

    jkjkjkjk

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 
 export default function Document() {
@@ -16,9 +17,36 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Prompt:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-0Q6JE2208E"/>
+<script
+dangerouslySetInnerHTML={{
+  __html:`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0Q6JE2208E');
+  `
+}}
+/> */}
+
+
+ 
+
+
+
       </Head>
       <body>
-       
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-0Q6JE2208E" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-0Q6JE2208E');
+        `}
+      </Script>
         <Main />
         <NextScript />
       fhdhfududhfudhfhud
